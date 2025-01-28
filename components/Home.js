f6847d675d8ffd1ee5e147f54fa1f36b7a0ee8c3
RishieVyas <rishievyas@gmail.com>
2025-01-28 02:41:11 -0500

    Displaying global state value using context state management

--- components/Home.js
+++ components/Home.js
@@ -1,17 +1,31 @@
 import { Image, Text, TouchableOpacity, View } from "react-native";
-import React from "react";
+import React, { useContext } from "react";
 import { IMAGE } from "../constants/IMAGE";
 import { CONSTANT } from "../constants/CONSTANTS";
 import LoginStyles from "../styles/styles";
+import AuthContext from "../utils/AuthContext";
 
 const Home = ({ navigation }) => {
+  const { state, dispatch } = useContext(AuthContext);
+
+  const onLogOutPressed = () => {
+    dispatch({ type: "LOGIN", payload: null });
+    navigation.navigate("Login");
+  };
+
   return (
     <View style={LoginStyles.homeContainer}>
-      <TouchableOpacity onPress={() => navigation.navigate("Login")}>
+      <Text style={{ fontSize: 20 }}>{CONSTANT.WELCOME_TEXT}</Text>
+
+      {/* Email displayed through context state management */}
+      {state.isAuthenticated ? (
+        <Text style={{ fontSize: 20 }}>Hello {state.user.email}</Text>
+      ) : null}
+
+      <Image style={{ marginBottom: 20 }} source={IMAGE.WELCOME_LOGO} />
+      <TouchableOpacity onPress={onLogOutPressed}>
         <Text style={LoginStyles.logOutText}>{CONSTANT.LOGOUT}</Text>
       </TouchableOpacity>
-      <Image style={{ marginBottom: 20 }} source={IMAGE.WELCOME_LOGO} />
-      <Text style={{ fontSize: 20 }}>{CONSTANT.WELCOME_TEXT}</Text>
     </View>
   );
 };
